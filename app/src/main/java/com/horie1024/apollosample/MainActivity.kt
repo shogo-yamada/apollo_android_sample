2309f266b1e8bd243adf6d517e039647516b5e06
horie1024 <ryosuke.pic@gmail.com>
2019-01-16 18:00:06 +0900

    first commit

---
+++ app/src/main/java/com/horie1024/apollosample/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.horie1024.apollosample
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
