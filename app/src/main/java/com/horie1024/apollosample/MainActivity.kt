f27b67ada2eca2f7d9b5b2b39c825fdd06142ed5
horie1024 <ryosuke.pic@gmail.com>
2019-01-17 11:17:50 +0900

    Apolloでのリクエストをテスト実装

--- app/src/main/java/com/horie1024/apollosample/MainActivity.kt
+++ app/src/main/java/com/horie1024/apollosample/MainActivity.kt
@@ -1,12 +1,50 @@
 package com.horie1024.apollosample
 
-import androidx.appcompat.app.AppCompatActivity
+import ShowLoginUserQuery
 import android.os.Bundle
+import android.util.Log
+import androidx.appcompat.app.AppCompatActivity
+import com.apollographql.apollo.ApolloCall
+import com.apollographql.apollo.ApolloClient
+import com.apollographql.apollo.api.Response
+import com.apollographql.apollo.exception.ApolloException
+import okhttp3.OkHttpClient
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private const val BASE_URL = "https://api.github.com/graphql"
+        private const val GITHUB_ACCESS_TOKENS = ""
+    }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val okHttpClient = OkHttpClient.Builder()
+            .authenticator { _, response ->
+                response.request().newBuilder()
+                    .addHeader("Authorization", "Bearer $GITHUB_ACCESS_TOKENS")
+                    .build()
+            }
+            .build()
+
+        val apolloClient = ApolloClient.builder()
+            .serverUrl(BASE_URL)
+            .okHttpClient(okHttpClient)
+            .build()
+
+        val query = ShowLoginUserQuery.builder().build()
+
+        apolloClient.query(query).enqueue(object : ApolloCall.Callback<ShowLoginUserQuery.Data>() {
+            override fun onFailure(e: ApolloException) {
+                e.printStackTrace()
+            }
+
+            override fun onResponse(response: Response<ShowLoginUserQuery.Data>) {
+                val viewer = response.data()?.viewer()
+                Log.i("test login user", viewer?.login())
+            }
+        })
     }
 }
